574d7b64d7532a86565dd37f7fade101148f1f18
k-nautresurrect <kushignite@gmail.com>
2022-01-25 11:04:05 +0530

    modified main.py according to classes

--- pythonvenv/main.py
+++ pythonvenv/main.py
@@ -1,4 +1,29 @@
-class Main:
-	def printHello(self, name):
-		print(f'hello {name}')
-		print(f'from main: {__name__}')
+# everything in python is an object
+x = 9
+print(type(x)) # class int
+
+# there is also an class of object
+a = object
+print(a)
+
+print(isinstance(x,object)) # everything in python is object
+
+print(isinstance(__name__,object))
+
+# every object has a flag wether the objects are callable or not
+# iscallable -> function | execute the code, class | makes an instance 
+
+# only object of instance A
+class A:
+	pass
+a = A
+print(a);
+print(id(a)); # the id of a
+print(hex(id(a))) # the actual adress of the instance
+
+# instance of instance of the object A
+b = A()
+print(b);
+print(id(b));
+print(hex(id(b))); 
+
--- pythonvenv/name.py
+++
@@ -1,5 +0,0 @@
-import main
-
-person = main.Main()
-person.printHello('from another module')
-print(f'from name : {__name__}')
